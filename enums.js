d0991a03861d08c6be6259c400bb44f2abd26a7c
wangzhipeng <wangzhipeng@hnnewsoft.com>
2023-01-17 20:53:39 +0800

    feat: 修改配置

--- enums.js
+++ enums.js
@@ -1,5 +1,5 @@
 // 模版名 - 对应github地址
 export const TEMPLATES = {
   "vue3-vite-ts": "AnterWang/peace-template-vue3-vite-ts",
-  "vue3-webpack-ts": "AnterWang/peace-template-vue3-webpack-ts"
+  // "vue3-webpack-ts": "AnterWang/peace-template-vue3-webpack-ts" // todo
 }
